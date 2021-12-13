04549bd60ad87f08da537a40fc673d2c515b6566
Bryce Champaign <brycechampaign@gmail.com>
2021-12-12 22:29:50 -0500

    Add different textures to each cube face

--- frontend/components/Cube.jsx
+++ frontend/components/Cube.jsx
@@ -1,22 +1,34 @@
 import React, { useRef, useState, Suspense } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three/src/loaders/TextureLoader.js";
-import faceReference from "../textures/face.jpg";
+import faceInnerReference from "../textures/face_inner.jpeg";
+import faceLeftReference from "../textures/face_left.jpeg";
+import faceRightReference from "../textures/face_right.jpg";
+import faceTopReference from "../textures/face_top.jpg";
+import faceBackReference from "../textures/face_back.jpg";
 
 const Cube = (props) => {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef();
-  // Hold state for hovered and clicked events
+
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => (ref.current.rotation.y += 0.01));
-  // Return the view, these are regular Threejs elements expressed in JSX
 
-  const face_1 = useLoader(TextureLoader, faceReference);
+  const faceInner = useLoader(TextureLoader, faceInnerReference);
+  const faceLeft = useLoader(TextureLoader, faceLeftReference);
+  const faceTop = useLoader(TextureLoader, faceTopReference);
+  const faceRight = useLoader(TextureLoader, faceRightReference);
+  const faceBack = useLoader(TextureLoader, faceBackReference);
 
   return (
     <mesh {...props} ref={ref} scale={3}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" map={face_1} args={[1, 1, 1]} />
+      <meshStandardMaterial attachArray="material" map={faceInner} />
+      <meshStandardMaterial attachArray="material" map={faceBack} />
+      <meshStandardMaterial attachArray="material" map={faceTop} />
+      <meshStandardMaterial attachArray="material" color={"red"} />
+      <meshStandardMaterial attachArray="material" map={faceLeft} />
+      <meshStandardMaterial attachArray="material" map={faceRight} />
     </mesh>
   );
 };
