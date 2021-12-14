c74d3a97b3301da7f05a424fb77775775d9c5766
Bryce Champaign <brycechampaign@gmail.com>
2021-12-13 20:31:41 -0500

    Implement header subtitle

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,7 +6,11 @@ import WIP from "./WIP";
 const App = () => {
   return (
     <>
-      <h1 id="name">&lt;Bryce Champaign /&gt;</h1>
+      <h1 id="name">&lt;BryceChampaign /&gt;</h1>
+      <h2 className="no-margin-bottom grow" id="name-subtitle">
+        <span className="tag">&lt;h2&gt;</span> Welcome to the Metaverse{" "}
+        <span className="tag">&lt;/h2&gt;</span>
+      </h2>
       <Cube />
       <WIP />
     </>
